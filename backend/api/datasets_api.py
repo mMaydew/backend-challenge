4bd0ad3889ca11efe770968e68cc5ed07add2960
mMaydew <m.maydew@protonmail.com>
2023-01-02 21:08:54 +0100

    Create docker files

---
+++ backend/api/datasets_api.py
@@ -0,0 +1,18 @@
+def list_datasets():
+    return {}
+
+
+def create_dataset(dataset):
+    return {}
+
+
+def get_dataset(id):
+    return {}
+
+
+def delete_dataset(id):
+    return {}
+
+
+def export_dataset(id):
+    return {}
