4a77b2a998c6f730ec4371d7f54789deb259f9ec
mMaydew <m.maydew@protonmail.com>
2023-01-02 23:08:26 +0100

    Update spec file to fix uploading datasets

--- backend/api/datasets_api.py
+++ backend/api/datasets_api.py
@@ -2,17 +2,17 @@ def list_datasets():
     return {}
 
 
-def create_dataset(dataset):
-    return {}
+def create_dataset(body):
+    return {"dataset": body}
 
 
 def get_dataset(id):
-    return {}
+    return {"id": id}
 
 
 def delete_dataset(id):
-    return {}
+    return {"id": id}
 
 
 def export_dataset(id):
-    return {}
+    return {"id": id}
