4c21a1c21cd6b07e70552884565cadecf00237f0
mMaydew <m.maydew@protonmail.com>
2023-01-02 21:08:54 +0100

    Create docker files

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,8 +1,12 @@
 from connexion import App
 
 
-app = App(__name__, specification_dir="./")
-app.add_api("api-spec.yml")
+app = App(
+    __name__,
+    specification_dir="./",
+    options={"swagger_ui": True},
+)
+app.add_api("./api-spec.yml")
 
 if __name__ == "__main__":
-    app.run(port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080)
